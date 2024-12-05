4b686547dd2e771920c0f62a00a43abb8027b881
Cristian Victoria <cristian.victoria.perez@gmail.com>
2024-12-05 18:37:47 -0500

    tar file plus first touches

---
+++ hashtable.hpp
@@ -0,0 +1,82 @@
+#ifndef HASHTABLE_HPP
+#define HASHTABLE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+
+#include "hashtable.h"
+
+using namespace std;
+
+//------------------------------------------------
+// ========== Constructors & Destructors =========
+//------------------------------------------------
+
+
+
+//------------------------------------------------
+// =========== Public Member functions ===========
+//------------------------------------------------
+
+
+
+//------------------------------------------------
+// =========== Private Member functions ==========
+//------------------------------------------------
+
+// returns largest prime number <= n or zero if input is too large
+// This is likely to be more efficient than prime_above(), because
+// it only needs a vector of size n
+template <typename K, typename V>
+  unsigned long HashTable<K, V>::prime_below (unsigned long n)
+  {
+    if (n > max_prime){
+        cerr << "** input too large for prime_below()\n";
+        return 0;
+    }
+    if (n == max_prime){
+        return max_prime;
+    }
+    if (n <= 1){
+      cerr << "** input too small \n";
+      return 0;
+    }
+
+    // now: 2 <= n < max_prime
+    vector <unsigned long> v (n+1);
+    setPrimes(v);
+    while (n > 2)
+      {
+        if (v[n] == 1)
+    return n;
+        --n;
+      }
+
+    return 2;
+  }
+
+//Sets all prime number indexes to 1. Called by method prime_below(n) 
+template <typename K, typename V>
+  void HashTable<K, V>::setPrimes(std::vector<unsigned long>& vprimes)
+  {
+    int i = 0;
+    int j = 0;
+
+    vprimes[0] = 0;
+    vprimes[1] = 0;
+    int n = vprimes.capacity();
+
+    for (i = 2; i < n; ++i)
+      vprimes[i] = 1;
+
+    for( i = 2; i*i < n; ++i)
+      {
+        if (vprimes[i] == 1)
+          for(j = i + i ; j < n; j += i)
+            vprimes[j] = 0;
+      }
+  }
+
+#endif
