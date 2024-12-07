60aeb1cbcd5a2e95c8d6ee1247bf4a25fda6f63c
Cristian Victoria <cristian.victoria.perez@gmail.com>
2024-12-06 19:05:02 -0500

    passserver

--- passserver.cpp
+++ passserver.cpp
@@ -4,69 +4,91 @@
 #include "passserver.h"
 #include <unistd.h> // For crypt()
 
-PassServer::PassServer(size_t size) : table(size) {}
+namespace cop4530{
+    // Constructor, create a hash table of the specified size. You just need to pass this size parameter to the constructor of the HashTable. 
+    // Therefore, the real hash table size could be different from the parameter size (because prime_below() will be called in the constructor 
+    // of the HashTable).
+    PassServer::PassServer(size_t size) : table(size) {}
 
-PassServer::~PassServer() {
-    table.clear();
-}
-
-bool PassServer::load(const char *filename) {
-    return table.load(filename);
-}
-
-bool PassServer::addUser(std::pair<std::string, std::string> &kv) {
-    kv.second = encrypt(kv.second);
-    return table.insert(kv);
-}
-
-bool PassServer::addUser(std::pair<std::string, std::string> &&kv) {
-    kv.second = encrypt(kv.second);
-    return table.insert(std::move(kv));
-}
-
-bool PassServer::removeUser(const std::string &k) {
-    return table.remove(k);
-}
-
-bool PassServer::changePassword(const std::pair<std::string, std::string> &p, const std::string &newpassword) {
-    if (newpassword == p.second) return false; // New password same as old password
-
-    auto encryptedOldPassword = encrypt(p.second);
-    auto encryptedNewPassword = encrypt(newpassword);
-
-    if (!table.match({p.first, encryptedOldPassword})) return false; // Old password mismatch
-
-    return table.insert({p.first, encryptedNewPassword}); // Update password
-}
-
-bool PassServer::find(const std::string &user) const {
-    return table.contains(user);
-}
-
-void PassServer::dump() const {
-    table.dump();
-}
-
-size_t PassServer::size() const {
-    return table.size();
-}
-
-bool PassServer::write_to_file(const char *filename) const {
-    return table.write_to_file(filename);
-}
-
-std::string PassServer::encrypt(const std::string &str) {
-    const char *salt = "$1$########";
-    char *encrypted = crypt(str.c_str(), salt);
-    if (!encrypted) {
-        throw std::runtime_error("Encryption failed");
+    // Destructor. You need to decide what you should do based on your design of PassServer (how you develop the adaptor class based on the 
+    // adaptee HashTable). In essence, we do not want to have memory leak.
+    PassServer::~PassServer() {
+        table.clear();
     }
 
-    std::string result(encrypted);
-    auto pos = result.find_last_of('$');
-    if (pos == std::string::npos || pos + 1 >= result.size()) {
-        throw std::runtime_error("Unexpected encryption format");
+    // Load a password file into the HashTable object. Each line contains a pair of username and encrypted password.
+    bool PassServer::load(const char *filename) {
+        return table.load(filename);
     }
 
-    return result.substr(pos + 1);
-}
+    // Add a new username and password.  The password passed in is in plaintext, it should be encrypted before insertion. The pair should 
+    // not be added if the username already exists in the hash table.
+    bool PassServer::addUser(std::pair<std::string, std::string> &kv) {
+        kv.second = encrypt(kv.second);
+        return table.insert(kv);
+    }
+
+    // Move version of addUser.
+    bool PassServer::addUser(std::pair<std::string, std::string> &&kv) {
+        kv.second = encrypt(kv.second);
+        return table.insert(std::move(kv));
+    }
+
+    // Delete an existing user with username k.
+    bool PassServer::removeUser(const std::string &k) {
+        return table.remove(k);
+    }
+
+    // Change an existing user's password. Note that both passwords passed in are in plaintext. They should be encrypted before you 
+    // interact with the hash table. If the user is not in the hash table, return false. If p.second does not match the current password, 
+    // return false. Also return false if the new password and the old password are the same (i.e., we cannot update the password).
+    bool PassServer::changePassword(const std::pair<std::string, std::string> &p, const std::string &newpassword) {
+        if (newpassword == p.second) return false; // New password same as old password
+
+        auto encryptedOldPassword = encrypt(p.second);
+        auto encryptedNewPassword = encrypt(newpassword);
+
+        if (!table.match({p.first, encryptedOldPassword})) return false; // Old password mismatch
+
+        return table.insert({p.first, encryptedNewPassword}); // Update password
+    }
+
+    // Check if a user exists (if user is in the hash table).
+    bool PassServer::find(const std::string &user) const {
+        return table.contains(user);
+    }
+
+    // Show the structure and contents of the HashTable object to the screen. Same format as the dump() function in the HashTable class template.
+    void PassServer::dump() const {
+        table.dump();
+    }
+
+    // Return the size of the HashTable (the number of username/password pairs in the table).
+    size_t PassServer::size() const {
+        return table.size();
+    }
+
+    // Save the username and password combination into a file. Same format as the write_to_file() function in the HashTable class template.
+    bool PassServer::write_to_file(const char *filename) const {
+        return table.write_to_file(filename);
+    }
+
+    // Encrypt the parameter str and return the encrypted string.
+    // Use the crypt() method to enctrypt the password, which should be MD5-based
+    std::string PassServer::encrypt(const std::string &str) {
+        const char *salt = "$1$########";
+        char *encrypted = crypt(str.c_str(), salt);
+        if (!encrypted) {
+            throw std::runtime_error("Encryption failed");
+        }
+
+        std::string result(encrypted);
+        auto pos = result.find_last_of('$');
+        if (pos == std::string::npos || pos + 1 >= result.size()) {
+            throw std::runtime_error("Unexpected encryption format");
+        }
+
+        return result.substr(pos + 1);
+    }
+
+};
--- passserver.h
+++ passserver.h
@@ -11,28 +11,27 @@ namespace cop4530 {
     class PassServer {
         public:
             // Constructors and Destructors
-            PassServer(size_t size = 101);
-            ~PassServer();
+            PassServer(size_t size = 101);  // Constructor
+            ~PassServer();  // Destructor
 
             // Public member functions
-            bool load(const char *filename);
-            bool addUser(std::pair<string, string> &kv);
-            bool addUser(std::pair<string, string> &&kv);
-            bool removeUser(const string &k);
-            bool changePassword(const std::pair<string, string> &p, const string & newpassword);
-            bool find(const string & user) const;
-            void dump() const;
-            size_t size() const;
-            bool write_to_file(const char *filename) const;
+            bool load(const char *filename);    // Load a password file into the HashTable object
+            bool addUser(std::pair<string, string> &kv);    // Add a new username and password
+            bool addUser(std::pair<string, string> &&kv);   // Move version of addUser
+            bool removeUser(const string &k);   // Delete an existing user with username k
+            bool changePassword(const std::pair<string, string> &p, const string & newpassword);    // Change an existing user's password
+            bool find(const string & user) const;   // Check if a user exists
+            void dump() const;  // Show the structure and contents of the HashTable object to the screen
+            size_t size() const;    // Return the size of the HashTable
+            bool write_to_file(const char *filename) const; // Save the username and password combination into a file
 
         private:
             // Private member functions
-            string encrypt(const string &str);
+            string encrypt(const string &str);  // Encrypt the parameter str and return the encrypted string
             
             // Private member data
             HashTable<string, string> table;
         };
-    #include "passserver.cpp"
 }
 
 #endif
